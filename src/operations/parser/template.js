b26457f77321334e8514c07795fe67f2689231ae
Matheus Almeida <tthheusalmeida@gmail.com>
2024-04-30 09:12:21 -0300

    feat: add get template content

--- src/operations/parser/template.js
+++ src/operations/parser/template.js
@@ -3,9 +3,14 @@
 const {
   compile
 } = require('vue-template-compiler');
+const {
+  getTemplateContent,
+} = require('../../utils/string');
 
 function getTemplateAst(fileContent = '') {
-  return compile(fileContent);
+  const template = getTemplateContent(fileContent);
+
+  return compile(template);
 }
 
 module.exports = {
--- src/utils/string.js
+++ src/utils/string.js
@@ -4,13 +4,41 @@ function replaceExtensionVueToJson(string) {
   return string.replace(/\.vue$/, ".json");
 }
 
+function getTagContent(fileContent, startTag, endTag, includeTag = false) {
+  const indexStart = fileContent.indexOf(startTag);
+  const indexEnd = fileContent.indexOf(endTag);
+
+  if (includeTag) {
+    return fileContent.substring(
+      indexStart,
+      indexEnd + endTag.length,
+    ).trim();
+  }
+  return fileContent.substring(
+    indexStart + startTag.length,
+    indexEnd,
+  ).trim();
+}
+
+function getTemplateContent(fileContent) {
+  return getTagContent(
+    fileContent,
+    '<template>',
+    '</template>',
+    true,
+  );
+}
+
 function getScriptContent(fileContent) {
-  const scriptStart = fileContent.indexOf('<script>');
-  const scriptEnd = fileContent.indexOf('</script>');
-  return fileContent.substring(scriptStart + '<script>'.length, scriptEnd).trim();
+  return getTagContent(
+    fileContent,
+    '<script>',
+    '</script>'
+  );
 }
 
 module.exports = {
   replaceExtensionVueToJson,
+  getTemplateContent,
   getScriptContent,
 }
