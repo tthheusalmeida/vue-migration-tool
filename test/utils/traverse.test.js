15cb51995044953530cba2411c3c00c4331b74b0
Matheus Almeida <tthheusalmeida@gmail.com>
2025-01-27 21:00:28 -0300

    test: move test transformer to utils/transformer test

--- test/utils/traverse.test.js
+++ test/utils/traverse.test.js
@@ -1,52 +1,49 @@
-const {
-  traverseTemplate,
-  walk,
-} = require('../../../../src/utils/traverse');
+const { traverseTemplate, walk } = require("../../src/utils/traverse");
 
-describe('=> operations/transformer/template/traverse', () => {
-  describe('traverseTemplate()', () => {
-    test('When passes a AST without node, should not traverse on AST.', () => {
+describe("=> operations/transformer/template/traverse", () => {
+  describe("traverseTemplate()", () => {
+    test("When passes a AST without node, should not traverse on AST.", () => {
       traverseTemplate();
 
       expect().toEqual();
     });
 
-    test('When passes a AST with node and action, should walk and transforme AST.', () => {
+    test("When passes a AST with node and action, should walk and transforme AST.", () => {
       const ast = {
         type: 1,
-        tag: 'div',
+        tag: "div",
         children: [
           {
             type: 2,
-            tag: 'span',
+            tag: "span",
             children: [
               {
-                type: 3
-              }
+                type: 3,
+              },
             ],
-          }
-        ]
+          },
+        ],
       };
       const action = (node) => {
-        if (node.tag === 'div') {
-          node.tag = 'a';
+        if (node.tag === "div") {
+          node.tag = "a";
         }
       };
 
       const expected = {
         type: 1,
-        tag: 'a',
+        tag: "a",
         children: [
           {
             type: 2,
-            tag: 'span',
+            tag: "span",
             children: [
               {
-                type: 3
-              }
+                type: 3,
+              },
             ],
-          }
-        ]
+          },
+        ],
       };
       traverseTemplate(ast, { action });
 
@@ -54,99 +51,99 @@ describe('=> operations/transformer/template/traverse', () => {
     });
   });
 
-  describe('walk()', () => {
-    test('When passes a AST without node, should not walk on AST.', () => {
+  describe("walk()", () => {
+    test("When passes a AST without node, should not walk on AST.", () => {
       walk();
 
       expect().toEqual();
     });
 
-    test('When passes a AST with node, should walk on AST.', () => {
+    test("When passes a AST with node, should walk on AST.", () => {
       const ast = {
         type: 1,
-        tag: 'div',
+        tag: "div",
         children: [
           {
             type: 2,
-            tag: 'span',
+            tag: "span",
             children: [
               {
-                type: 3
-              }
+                type: 3,
+              },
             ],
-          }
-        ]
+          },
+        ],
       };
       const expected = {
         type: 1,
-        tag: 'div',
+        tag: "div",
         children: [
           {
             type: 2,
-            tag: 'span',
+            tag: "span",
             children: [
               {
-                type: 3
-              }
+                type: 3,
+              },
             ],
-          }
-        ]
+          },
+        ],
       };
       walk(ast);
 
       expect(ast).toEqual(expected);
     });
 
-    test('When passes a AST with node and action, should walk and transforme AST.', () => {
+    test("When passes a AST with node and action, should walk and transforme AST.", () => {
       const ast = {
         type: 1,
-        tag: 'div',
+        tag: "div",
         children: [
           {
             type: 2,
-            tag: 'span',
+            tag: "span",
             children: [
               {
-                type: 3
-              }
+                type: 3,
+              },
             ],
-          }
-        ]
+          },
+        ],
       };
       const action = (node) => {
-        if (node.tag === 'div') {
-          node.tag = 'a';
+        if (node.tag === "div") {
+          node.tag = "a";
         }
       };
 
       const expected = {
         type: 1,
-        tag: 'a',
+        tag: "a",
         children: [
           {
             type: 2,
-            tag: 'span',
+            tag: "span",
             children: [
               {
-                type: 3
-              }
+                type: 3,
+              },
             ],
-          }
-        ]
+          },
+        ],
       };
       walk(ast, action);
 
       expect(ast).toEqual(expected);
     });
 
-    test('When passes a AST with node and no action, should walk on AST without transforming.', () => {
+    test("When passes a AST with node and no action, should walk on AST without transforming.", () => {
       const ast = {
         type: 1,
-        tag: 'div',
+        tag: "div",
       };
       const expected = {
         type: 1,
-        tag: 'div',
+        tag: "div",
       };
       walk(ast);
 
