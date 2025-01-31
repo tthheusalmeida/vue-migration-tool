0da9e08dc7c3c5c4b46b7eb8356a4f2256770cd9
Matheus Almeida <tthheusalmeida@gmail.com>
2025-01-30 21:39:15 -0300

    test: add test for singletons/stateManager

---
+++ test/singletons/stateManager.test.js
@@ -0,0 +1,47 @@
+const StateManager = require("./../../src/singletons/stateManager");
+
+describe("StateManager Singleton", () => {
+  beforeEach(() => {
+    StateManager.reset();
+  });
+
+  test("should initialize all properties as null", () => {
+    expect(StateManager.getState()).toEqual({
+      importVue: null,
+      importVuex: null,
+      importVueRouter: null,
+      appVariableDeclaration: null,
+      newVueOtherProps: null,
+      newVueMount: null,
+      routerPropMode: null,
+    });
+  });
+
+  test("should allow setting and getting properties", () => {
+    StateManager.set("importVue", { type: "ImportDeclaration" });
+    expect(StateManager.get("importVue")).toEqual({
+      type: "ImportDeclaration",
+    });
+  });
+
+  test("should reset all properties to null", () => {
+    StateManager.set("importVue", { type: "ImportDeclaration" });
+    StateManager.set("routerPropMode", { value: "history" });
+    StateManager.reset();
+    expect(StateManager.getState()).toEqual({
+      importVue: null,
+      importVuex: null,
+      importVueRouter: null,
+      appVariableDeclaration: null,
+      newVueOtherProps: null,
+      newVueMount: null,
+      routerPropMode: null,
+    });
+  });
+
+  test("should maintain singleton instance", () => {
+    const instance1 = require("../stateManager");
+    const instance2 = require("../stateManager");
+    expect(instance1).toBe(instance2);
+  });
+});
